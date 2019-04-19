bb54da0915a2461d9dc193f31045e256630e23cb
zemrlab <zoraida.emperatriz@gmail.com>
2019-04-19 00:53:03 -0500

    version 1.2

---
+++ src/Configuracion/Config1.js
@@ -0,0 +1,4 @@
+//conexion al modulo de asignacion de recibos
+const CONFIG1= 'https://sigadev1-consultarecibos-back.herokuapp.com/'
+//const CONFIG= 'http://localhost:8080/';
+export default CONFIG1;

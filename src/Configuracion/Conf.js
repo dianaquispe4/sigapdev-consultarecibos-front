75e567b2023eab762d893a0b94bedeb67f84bd4b
proyectosigap <zmamanir@unmsm.edu.pe>
2020-07-27 02:35:06 -0500

    version 1.4.4

--- src/Configuracion/Conf.js
+++ src/Configuracion/Conf.js
@@ -1,5 +1,4 @@
-
+	//No realizar cambios a este archivo
    const CONFIG1= 'https://sigapdev-conrecibospre-back.herokuapp.com'   
-   //const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
 
 export default CONFIG1;
